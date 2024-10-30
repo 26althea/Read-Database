c065ae62fba986d609e8846df13820118e974c16
LOOOOOONNEER <paduabeloved5@gmail.com>
2024-10-30 23:21:22 +0800

    Update taba.java
    update/backup before experimenting\

--- Peta/taba.java
+++ Peta/taba.java
@@ -3,25 +3,21 @@ import java.io.FileReader;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.io.BufferedWriter;
 
 public class taba {
-  private static boolean notDone = true;
-
+  private static  boolean notDone = true;
+  
 
   public void isDone(boolean notDone){
     this.notDone = notDone;
-  }
-  public boolean getValue(){
-    return notDone;
   }
 
  public static void main(String[] args) {
   Scanner s = new Scanner(System.in);
   dat dat = new dat();
-  
-  
 
-    
+
     while (notDone) {
       System.out.println("Enter the role you want to search for: *D - Doctor | P - Patient");
       String role = s.nextLine();
@@ -38,12 +34,13 @@ public class taba {
 
       break;
         default:
+        System.out.println("Please enter a valid option");
           break;
       }
 
     
   }
-
+s.close();
 
  }   
 }
@@ -58,11 +55,11 @@ String role = "";
 String folderPath = "Downloads\\Peta\\Database\\" + role;
 
 
-//Doctors
+//Doctors file connected variables
      String fileName = "Oraya";  
       String doctorsPath = folderPath + "\\" + fileName + ".txt";
       File dFile = new File(doctorsPath);
-     // Patient
+     // Patient file connected variables
       String patientNum = "0";
       String diagnosis;
       String patientPath = folderPath+"\\"+fileName+"-"+patientNum+"-"+diagnosis+ ".txt";
@@ -72,20 +69,19 @@ String folderPath = "Downloads\\Peta\\Database\\" + role;
           this.fileName = newfileName;
           this.folderPath = "Downloads\\Peta\\Database\\" + role;
           this.doctorsPath = folderPath + "\\" + fileName + ".txt";
-          this.dFile = new File(doctorsPath);
+          this.dFile = new File(doctorsPath); 
       }
       public void pSetFileName(String newFileName){
         this.fileName = newFileName;
         this.folderPath = "Downloads\\Peta\\Database\\" + role;
         this.patientPath = folderPath+"\\"+fileName+"-"+patientNum+"-"+diagnosis+ ".txt";
         this.pFile = new File(patientPath);
-
-
       }
 
-
-
       public void searhDoctor(){
+        taba.isDone(true);
+        boolean ambaho = true;
+        while(ambaho){
       
         System.out.println("--------------------------------------");
         System.out.println("Search for a doctor\nEnter doctor's last name: ");
@@ -95,12 +91,19 @@ String folderPath = "Downloads\\Peta\\Database\\" + role;
         if(dFile.exists()){
           count = 0;
             readFile(dFile);
-            System.out.println("Are you now done looking for doctors? ");
+            System.out.println("Are you done looking for doctors? ");
             String ans = s.nextLine();
             switch (ans.toLowerCase()) {
                 case "yes":
-                taba.isDone(false);
+                System.out.println("Do you want to go back to mainscreen?");
+                String a = s.nextLine();
+                if(a.equalsIgnoreCase("yes")){
+                  ambaho = false;
             
+                }else{
+                  taba.isDone(false);
+                }
+              
                     
                     break;
                 case "no":
@@ -108,8 +111,7 @@ String folderPath = "Downloads\\Peta\\Database\\" + role;
                 break;
             
                 default:
-                System.out.println("Not a valid option.\nEnding process now.");
-                taba.isDone(false);
+               
                     break;
             }
 
@@ -119,9 +121,7 @@ String folderPath = "Downloads\\Peta\\Database\\" + role;
           System.out.println("Please try again");
 
           count++;
-          System.out.println(taba.getValue());
-          System.out.println(count);
-
+        
         }
        if(count >=3 ){
         System.out.println("Do you still want to continue? ");
@@ -133,8 +133,12 @@ String folderPath = "Downloads\\Peta\\Database\\" + role;
         }else{
           System.out.println("ending process now.");
           taba.isDone(false);
+          ambaho = false;
+          
+          
+          
         }
-      }
+      }}
       }
 
 
@@ -162,31 +166,54 @@ String folderPath = "Downloads\\Peta\\Database\\" + role;
 
 
       public void searchPatient(){
+      
+        boolean mabango = true;
+        while(mabango){
         System.out.println("--------------------------------------");
         System.out.println("Search for patient\nEnter Patient's last name and First Name:");
         System.out.println("(Click Enter before entering First Name)");
-        String last = s.nextLine();
-        String first = s.nextLine();
+        String last = s.nextLine().trim();
+        String first = s.nextLine().trim()  ;
+        fileName = last.toUpperCase() + ","+first.toUpperCase();
+        pSetFileName(fileName);
+        
+       
+        File path = new File(folderPath);
+        String []listOfN = path.list();
+       
+        for(String names: listOfN){
+          if(names.startsWith(fileName)){
+            System.out.println(names);
+         
+          }
+        }
+
         System.out.println("Enter Patient's ID number: \nex.000001");
         patientNum = s.nextLine();
+
+
        
         System.out.println("Enter doctor's diagnosis: ");
         diagnosis = s.nextLine();
-
-        fileName = last + ","+first;
+        diagnosis.toUpperCase();
+      
+         
         pSetFileName(fileName);
-        readFile(pFile);
+
         if(pFile.exists()){
-          System.out.println("tae ko mabaho");
+          readFile(pFile);
+
           taba.isDone(false);
+
         }else{
-          taba.isDone(false);
+          System.out.println(pFile);
+          System.out.println("You're looking for something that doesn't exists");
         }
 
         
       
       }
-
+    }
       
   
      
